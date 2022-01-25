fccc0e93b29c5e647759306538496b755fe34316
aarti <aartibhawsar283@gmail.com>
2022-01-25 10:03:41 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -4,11 +4,19 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+//import * as serviceWorker from "./serviceWorker";
+import { RoomProvider } from "./context";
+import { BrowserRouter } from "react-router-dom";
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <RoomProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </RoomProvider>
+  ,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
